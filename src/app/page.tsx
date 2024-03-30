bd28a3604ea3acff6364445800b16a24411f5fc9
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-03-30 22:24:28 +0530

    Initially project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import NavbarMain from "@/components/Navbar"
+
+export default function Home() {
+
+  return (
+    <main className="min-h-screen">
+      <NavbarMain />
+      <h1>Home</h1>
+    </main>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
+
+const NavbarMain = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Works",
+    "Contact",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+      </NavbarContent>
+
+
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default NavbarMain
