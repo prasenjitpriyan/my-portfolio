6b2a381be7e6f181d7da0e7b9eb3d6428e55d06c
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-03-31 00:18:47 +0530

    Header complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,35 @@
 "use client"
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
+import Link from 'next/link'
 
 const NavbarMain = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Works",
-    "Contact",
-  ];
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-      </NavbarContent>
-
-
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+    <header className="grid grid-cols-12 gap-4 ml-40 mr-40 mt-2">
+      <nav className="col-span-3 flex flex-row justify-start gap-4 mt-4">
+        <Link className="text-lg" href="works">Works</Link>
+        <Link className="text-lg" href="contact">Contact</Link>
+      </nav>
+      <div className="col-span-6 flex justify-center">
+        <Link href="/">
+          <img className="w-72" src='/logo.png' alt="My Logo" />
+        </Link>
+      </div>
+      <div className="col-span-3 flex flex-row justify-end gap-4 mt-4">
+        <Link href="/">
+          <img className="w-6" src="/linkedin.svg" alt="Linkedin" />
+        </Link>
+        <Link href="/">
+          <img className="w-6" src="/github.svg" alt="Github" />
+        </Link>
+        <Link href="/">
+          <img className="w-6" src="/stackoverflow.svg" alt="Stack Overflow" />
+        </Link>
+        <Link href="/">
+          <img className="w-6" src="dribbble.svg" alt="Dribble" />
+        </Link>
+      </div>
+    </header>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,15 @@
 import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
 export default config;
