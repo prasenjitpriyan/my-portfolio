ddf25cb1114ca4cb83101fae03c388e19cad40d6
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-29 19:27:17 +0530

    color change in slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Slider = () => {
   return (
     <div className="carousel w-full h-[80svh] rounded-md border-2 border-text-primary">
-      <div id="slide1" className="carousel-item relative w-full bg-red-100">
+      <div id="slide1" className="carousel-item relative w-full bg-[#F2F3F3]">
         <h1>Slide-1</h1>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <Link href="#slide4" className="btn btn-circle">
@@ -15,7 +15,7 @@ const Slider = () => {
           </Link>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
+      <div id="slide2" className="carousel-item relative w-full bg-[#F2F3F3]">
         <h1>Slide-2</h1>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <Link href="#slide1" className="btn btn-circle">
@@ -26,7 +26,7 @@ const Slider = () => {
           </Link>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
+      <div id="slide3" className="carousel-item relative w-full bg-[#F2F3F3]">
         <h1>Slide-3</h1>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <Link href="#slide2" className="btn btn-circle">
@@ -37,7 +37,7 @@ const Slider = () => {
           </Link>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full">
+      <div id="slide4" className="carousel-item relative w-full bg-[#F2F3F3]">
         <h1>Slide-4</h1>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <Link href="#slide3" className="btn btn-circle">
